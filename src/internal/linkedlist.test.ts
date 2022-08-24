21d58f386f35e9e6ebd76d1bbba0799028224bb0
Brian Duff <bduff@google.com>
2022-08-23 17:17:59 -0700

    Add missing dir

---
+++ src/internal/linkedlist.test.ts
@@ -0,0 +1,76 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { LinkedList } from "./linkedlist"
+
+test("Push and get elements back", () => {
+  const list = new LinkedList(5);
+  list.push(6);
+  list.push(7);
+  list.push(8);
+
+  expect(list.toArray()).toEqual([5, 6, 7, 8])
+})
+
+test("Insert after", () => {
+  const list = LinkedList.fromArray([1, 2, 3, 4, 5]);
+  list.insertAfter(list.getHead()?.next, 99)
+  expect(list.toArray()).toEqual([1, 2, 99, 3, 4, 5])
+})
+
+test("Insert after with undefined", () => {
+  const list = LinkedList.fromArray([1, 2, 3, 4, 5]);
+  list.insertAfter(undefined, 55)
+  expect(list.toArray()).toEqual([55, 1, 2, 3, 4, 5])
+})
+
+test("Insert after with undefined in empty list", () => {
+  const list = new LinkedList<number>();
+  list.insertAfter(undefined, 55)
+  expect(list.toArray()).toEqual([55])
+})
+
+test("Insert after tail", () => {
+  const list = LinkedList.fromArray([1, 2, 3]);
+  list.insertAfter(list.getTail(), 100);
+  expect(list.toArray()).toEqual([1, 2, 3, 100])
+})
+
+test("Remove head", () => {
+  const list = LinkedList.fromArray([1, 2, 3])
+  list.removeNode(list.getHead()!)
+  expect(list.toArray()).toEqual([2, 3])
+})
+
+test("Remove tail", () => {
+  const list = LinkedList.fromArray([1, 2, 3])
+  list.removeNode(list.getTail()!);
+  expect(list.toArray()).toEqual([1, 2])
+})
+
+test("Remove middle", () => {
+  const list = LinkedList.fromArray([5, 6, 7])
+  list.removeNode(list.getHead()?.next!);
+  expect(list.toArray()).toEqual([5, 7])
+})
+
+test("Remove node range", () => {
+  const list = LinkedList.fromArray([5, 6, 7, 8, 9, 10, 11, 12])
+
+  list.removeNodeRange(list.getHead()?.next, list.getTail()?.prev);
+
+  expect(list.toArray()).toEqual([5, 12])
+})
---
+++ src/internal/linkedlist.ts
@@ -0,0 +1,154 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @internal
+ */
+export class Node<T> {
+  prev?: Node<T>;
+  next?: Node<T>;
+  val: T;
+
+  constructor(value: T) {
+    this.val = value;
+  }
+}
+
+/**
+ * @internal
+ */
+export class LinkedList<T> {
+  private head: Node<T> | undefined
+  private tail: Node<T> | undefined
+
+  constructor(headValue?: T) {
+    const head = headValue ? new Node(headValue) : undefined;
+    this.head = head;
+    this.tail = head;
+  }
+
+  static fromArray<T>(array: T[]) {
+    const list = new LinkedList<T>();
+    for (const v of array) {
+      list.push(v);
+    }
+    return list;
+  }
+
+  private createNode(value: T) {
+    return new Node(value)
+  }
+
+  private insertNodeBefore(pos: Node<T>, node: Node<T>) {
+    node.next = pos;
+    node.prev = pos.prev;
+    if (pos.prev) {
+      pos.prev.next = node;
+    } else {
+      this.head = node;
+    }
+    pos.prev = node;
+  }
+
+  private insertNodeAfter(pos: Node<T>, node: Node<T>) {
+    node.prev = pos;
+    node.next = pos.next;
+    if (pos.next) {
+      pos.next.prev = node;
+    } else {
+      this.tail = node;
+    }
+    pos.next = node;
+  }
+
+  insertAfter(pos: Node<T> | undefined, value: T) {
+    const node = this.createNode(value);
+    if (!pos) {
+      if (this.head) {
+        this.insertNodeBefore(this.head, node);
+      } else {
+        this.head = node;
+        this.tail = node;
+      }
+    } else {
+      this.insertNodeAfter(pos, node);
+    }
+    return node;
+  }
+
+  removeNode(node: Node<T>) {
+    if (node.prev) {
+      node.prev.next = node.next;
+    } else {
+      this.head = node.next;
+    }
+
+    if (node.next) {
+      node.next.prev = node.prev;
+    } else {
+      this.tail = node.prev;
+    }
+  }
+
+  /**
+   * Removes a range of contiguously connected nodes.
+   */
+  removeNodeRange(start?: Node<T>, end?: Node<T>) {
+    // TODO: this should be optimized to just attach the beginning to the end.
+    // TODO: we should also actually check that they're contiguous
+    let from = start ?? this.head;
+    const to = end ?? this.tail
+
+    while (from) {
+      const next = from.next;
+      this.removeNode(from);
+      if (from === to) {
+        break;
+      }
+      from = next;
+    }
+  }
+
+  private pushNode(node: Node<T>) {
+    if (this.tail) {
+      this.insertNodeAfter(this.tail, node);
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+  }
+
+  push(value: T) {
+    this.pushNode(new Node(value))
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  toArray() {
+    const result = []
+    let n: Node<T> | undefined = this.head;
+    while (n) {
+      result.push(n.val);
+      n = n.next;
+    }
+    return result;
+  }
+}
+
